4248046c32fe7685d220d4070bcc4a0ffd312235
Ogg <ogg.developer@gmail.com>
2019-08-26 14:51:51 +0200

    Added moving left/right

--- map.py
+++ map.py
@@ -1,10 +1,11 @@
 import time
+import random
 mapWidth = 10
 mapHeight = 20
 class Map:
     def __init__(self):
         self.blockX = 1
-        self.blockY = 2
+        self.blockY = 15
         self.block = [
                             ['', 'r'],
                             ['', 'r'],
@@ -17,27 +18,6 @@ class Map:
                 line.append('')
             self.map.append(line)
 
-    def update(self):
-        def canExist(bx, by, x, y):
-            try:
-                mapTileIsFree = self.map[y][x] == ''
-                pass
-            finally:
-                pass
-            return mapTileIsFree
-
-        def removeTile(bx, by, x, y):
-            self.map[y][x] = ''
-
-        def placeTile(bx, by, x, y):
-            self.map[y][x] = self.block[by][bx]
-
-
-        self.forTileInBlock(removeTile)
-        self.blockY += 1
-        self.forTileInBlock(placeTile)
-
-
     def forTileInBlock(self, run):
         returnValues = []
         for by in range(len(self.block)):
@@ -47,10 +27,62 @@ class Map:
                     y = self.blockY + by
                     returnValue = run(bx, by, x, y)
                     returnValues.append(returnValue)
+        return returnValues
+
+    def canExist(self, bx, by, x, y):
+        if x < 0 or y < 0:
+            return False
+        mapTileIsFree = False
+        try:
+            mapTileIsFree = self.map[y][x] == ''
+            pass
+        except:
+            pass
+        return mapTileIsFree
+
+    def removeTile(self, bx, by, x, y):
+        self.map[y][x] = ''
+
+    def placeTile(self, bx, by, x, y):
+        self.map[y][x] = self.block[by][bx]
+
+    def tryMove(self, moveFunction, unmoveFunction):
+        self.forTileInBlock(self.removeTile)
+        moveFunction()
+        if not all(self.forTileInBlock(self.canExist)):
+            unmoveFunction()
+        self.forTileInBlock(self.placeTile)
+
+    def tryMoveDown(self):
+        self.forTileInBlock(self.removeTile)
+        self.blockY += 1
+        if not all(self.forTileInBlock(self.canExist)):
+            self.blockY -= 1
+        self.forTileInBlock(self.placeTile)
+
+    def tryMoveRight(self):
+        self.forTileInBlock(self.removeTile)
+        self.blockX += 1
+        if not all(self.forTileInBlock(self.canExist)):
+            self.blockX -= 1
+        self.forTileInBlock(self.placeTile)
+
+    def tryMoveLeft(self):
+        self.forTileInBlock(self.removeTile)
+        self.blockX -= 1
+        if not all(self.forTileInBlock(self.canExist)):
+            self.blockX += 1
+        self.forTileInBlock(self.placeTile)
+
+    def update(self):
+        if random.random()>0.5:
+            if random.random()>0.9:
+                self.tryMoveRight()
+            else:
+                self.tryMoveLeft()
+
+        self.tryMoveDown()
         
-
-
-
     def prettyPrint(self):
         print()
         for line in self.map:
@@ -61,11 +93,6 @@ class Map:
                 print(tile, end='')
             print()
         print()
-
-
-
-
-
 
 m = Map()
 m.prettyPrint()
