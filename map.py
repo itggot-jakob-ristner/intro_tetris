85db9f757867bba93e75498d00c74f6d5298dfba
Ogg <ogg.developer@gmail.com>
2019-08-26 15:50:32 +0200

    Added basic rotation

--- map.py
+++ map.py
@@ -4,7 +4,8 @@ mapWidth = 10
 mapHeight = 20
 class Map:
     def __init__(self):
-        self.blockX = 1
+        self.blockR = 0
+        self.blockX = 4
         self.blockY = 1
         self.block = [
                             ['', 'r'],
@@ -22,9 +23,25 @@ class Map:
         returnValues = []
         for by in range(len(self.block)):
             for bx in range(len(self.block[by])):
+                if self.blockR == 0:
+                    dx = bx
+                    dy = by
+                    pass
+                elif self.blockR == 1:
+                    dx = -by
+                    dy = bx
+                    pass
+                elif self.blockR == 2:
+                    dx = -bx
+                    dy = -by
+                    pass
+                elif self.blockR == 3:
+                    dx = by
+                    dy = -bx
+                    pass
                 if self.block[by][bx] != '':
-                    x = self.blockX + bx
-                    y = self.blockY + by
+                    x = self.blockX + dx
+                    y = self.blockY + dy
                     returnValue = run(bx, by, x, y)
                     returnValues.append(returnValue)
         return returnValues
@@ -74,8 +91,28 @@ class Map:
             self.blockX += 1
         self.forTileInBlock(self.placeTile)
 
+    def tryRotateRight(self):
+        self.forTileInBlock(self.removeTile)
+        self.blockR += 1
+        self.blockR %= 4
+        if not all(self.forTileInBlock(self.canExist)):
+            self.blockR -= 1
+            self.blockR %= 4
+        self.forTileInBlock(self.placeTile)
+
+    def tryRotateLeft(self):
+        self.forTileInBlock(self.removeTile)
+        self.blockR -= 1
+        self.blockR %= 4
+        if not all(self.forTileInBlock(self.canExist)):
+            self.blockR += 1
+            self.blockR %= 4
+        self.forTileInBlock(self.placeTile)
+
+
     def update(self):
         self.tryMoveDown()
+        self.tryRotateRight()
         
     def prettyPrint(self):
         print()
